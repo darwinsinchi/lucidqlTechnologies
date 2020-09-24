7442e05c9d26f0ae2387eb95d73fbe236c83454f
Darwin Sinchi <dsinchi12@gmail.com>
2020-09-24 09:52:20 -0400

    final version

--- src/components/secondbody.js
+++ src/components/secondbody.js
@@ -5,23 +5,30 @@ function Secondbody() {
     <div className="secondBody">
       <div className="firstBody">
         <div className="column">
-          <h1 className="howto">How to use lucidQL</h1>
+          <h1 className="howto">How does lucidQL work?</h1>
           <h4 className="text">
-            First select an existing Postgres URI that you want to use to create
-            a graphQL schema and paste that link into lucidQL and send it.
-            lucidQL will create a schema and provide you with the required files
-            to setup your back end with GraphQL, which includes a server file, a
-            connection file, and a schema file. lucidQL also allows the user to
-            simplify their schema by being able to delete any relationships they
-            no longer require by dragging a relationship into the garbage
-            container. The user can also undo a deletion.
+            First choose your desired Postgres database that you want to use to
+            create a graphQL API, and enter the link where prompted, and click
+            submit. lucidQL will immediately start by reading your database,
+            extracting all the tables, and their relationships, and generate a
+            GraphQL schema. A visual representation will display on the left
+            side of the tool, users can analyze their current database and their
+            table relationships.
           </h4>
           <h4 className="text">
-            The connection file connects your Postgres API to your server. The
-            server file sets up your server and includes playground so that you
-            can query the information from your API as needed. Lastly, your
-            schema file will provide you with queries, mutations, and resolvers
-            based on your Postgres URI.{" "}
+            The lucidQL tool allows the user to simplify their schema by being
+            able to delete any relationships that they no longer require.
+            <ul className="bulletpoints">
+              <li>
+                If any tables are undesired in the final product, simply drag a
+                table to the garbage icon, lucidQL will handle the rest.
+              </li>
+              <li>
+                The GraphQL schemas will be regenerated accordingly to your
+                changes.
+              </li>
+              <li>if you make a mistake, simply click the "undo" button.</li>
+            </ul>
           </h4>
         </div>
 
@@ -41,11 +48,11 @@ function Secondbody() {
           </a>
           <h3 className="optionA">Option A:</h3>
           <h4 className="text">
-            lucidQL also provides access to playgound to query an existing
-            database. The user will just have to click on "GraphQL PLayground"
-            which is found on the top left side of the page within the Navbar
-            Icon. After clicking this button, the user will be redirected to
-            playground.
+            The lucidQL tool comes pre-packaged with a backend, which enables
+            the user to access GraphQL playground and start querying your new
+            API, immediately. After entering a Postgres URI. The user will just
+            have to click on "GraphQL PLayground" which can be accessed through
+            the side menu bar.
           </h4>
         </div>
 
@@ -67,6 +74,23 @@ function Secondbody() {
           <h4 className="text">
             After the application is running, enter localhost:3000/playground in
             your browser and you can start querying your database
+          </h4>
+          <h4 className="text">
+            The downloaded package includes the following:
+            <ul>
+              <li>
+                A connection file connects your Postgres API to your server
+              </li>
+              <li>
+                The server file sets up your server and includes playground so
+                that you can query the information from your API as needed
+              </li>
+              <li>
+                Lastly, your schema file will provide you with queries,
+                mutations, and resolvers based on your pre-existing relational
+                database
+              </li>
+            </ul>
           </h4>
         </div>
         <code className="npmbox">
