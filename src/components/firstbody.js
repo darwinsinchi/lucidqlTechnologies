89284f14e0f05348377b864435d94241a5007b7d
Darwin Sinchi <dsinchi12@gmail.com>
2020-09-24 12:19:44 -0400

    final page

--- src/components/firstbody.js
+++ src/components/firstbody.js
@@ -3,20 +3,23 @@ import logoPicture from "../img/lucidQL.png";
 
 function FirstBody() {
   return (
-    <div className="firstBody">
-      <div className="column">
-        <h1 className="maintitle">
-          Build a fast, responsive, GraphQL API with lucidQL
-        </h1>
-        <h4 className="text">
-          Quickly design GraphQL APIs from existing RESTful APIs that use an
-          existing Postgres database.{" "}
-        </h4>
-        <form action="http://www.lucidql.com/">
-          <input className="gotolucid" type="submit" value="Try It Now" />
-        </form>
+    <div>
+      <div className="firstBody">
+        <div className="column">
+          <h1 className="maintitle">
+            Build a fast, responsive, GraphQL API with lucidQL
+          </h1>
+          <h4 className="text">
+            Quickly design GraphQL APIs from existing RESTful APIs that use an
+            existing Postgres database.{" "}
+          </h4>
+          <form action="http://www.lucidql.com/">
+            <input className="gotolucid" type="submit" value="Try It Now" />
+          </form>
+        </div>
+
+        <img className="logopicture" src={logoPicture} alt="picture"></img>
       </div>
-      <img className="logopicture" src={logoPicture} alt="picture"></img>
     </div>
   );
 }
